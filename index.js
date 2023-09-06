4b982aa3b04f5e977d484c728ce7057735908a2c
Giuliano Faustini <giuliperus@gmail.com>
2023-09-06 17:51:10 +0300

    Phonebook backend step3

--- index.js
+++ index.js
@@ -21,13 +21,11 @@ const persons = [
     id: 4,
     name: "Mary Poppendieck",
     number: "39-23-6423122",
-  }
+  },
 ];
 
+const howMany = () => persons.length;
 
-const howMany = () => persons.length
-
- 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -36,8 +34,23 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  
+});
+
 app.get("/info", (request, response) => {
-  response.send(`<p>Phonebook contains info for ${howMany()} people</p> <p>${new Date()}</p>`);
+  response
+    .send(
+      `<p>Phonebook contains info for ${howMany()} people</p> <p>${new Date()}</p>`
+    )
+    .end();
 });
 
 const PORT = 3001;
