d53cd1533c6f93307269552864462fe9f00e5551
Giuliano Faustini <giuliperus@gmail.com>
2023-09-07 08:37:43 +0300

    3.5: Phonebook backend step5 Added POST request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -54,15 +56,34 @@ app.get("/info", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-
   const personToDelete = persons.find((person) => person.id === id);
-
   if (!personToDelete) {
     response.status(404).end();
   } else {
     persons = persons.filter((person) => person.id !== id);
     response.status(204).end();
   }
+});
+const generatedId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name && !body.number) {
+    return response.status(400).json({
+      error: "content is missing",
+    });
+  }
+  const person = {
+    id: generatedId(),
+    name: body.name,
+    number: body.number
+  };
+  console.log(person)
+  persons.push(person);
+  response.json(person);
 });
 
 const PORT = 3001;
