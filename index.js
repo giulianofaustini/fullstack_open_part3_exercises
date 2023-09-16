2142865cb5fb86f6fac5fdf5e17e315acf826010
Giuliano Faustini <giuliperus@gmail.com>
2023-09-16 12:03:34 +0300

    3.21 Deploying the database backend to production

--- index.js
+++ index.js
@@ -169,7 +169,8 @@ app.post("/api/persons", (request, response) => {
     });
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, '0.0.0.0',() => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
