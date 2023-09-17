f6a4a6f7332708bdc6168a8ca7c64579da578a46
Giuliano Faustini <giuliperus@gmail.com>
2023-09-17 10:14:11 +0300

    ESLINT for Exercise 3.22.

--- models/person.js
+++ models/person.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -11,30 +12,30 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 8,
     validate: {
-      validator: function(v) {
-        return /\d{3}-\d{7}/.test(v);
+      validator(v) {
+        return /\d{3}-\d{5, 7}/.test(v)
       },
-      message: props => `${props.value} is not a valid phone number!`
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    required: [true, 'User phone number required']
+    required: [true, 'User phone number required'],
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
 mongoose
   .connect(url)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+    console.error('Error connecting to MongoDB:', error)
+  })
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
